c69cef1e3c4d4575295cff3572897c8df6fd428d
BYOUINZAKA <2606675531@qq.com>
2020-02-08 00:41:39 +0800

    beta-0.0.1

--- NameWheel/MlpNetwork.h
+++ NameWheel/MlpNetwork.h
@@ -5,7 +5,7 @@
 /**
  *  MIT License
  *  
- *  Copyright (c) 2020 Hata.Rotom https://github.com/BYOUINZAKA
+ *  Copyright (c) 2020 Byouinzaka¡¤KuroNeko https://github.com/BYOUINZAKA
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
