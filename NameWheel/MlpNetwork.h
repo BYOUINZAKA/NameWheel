d5fe7adede1844fe3fef8f09f7c26665a62c49fd
BYOUINZAKA <2606675531@qq.com>
2020-02-08 00:39:48 +0800

    beta-0.0.1

--- NameWheel/MlpNetwork.h
+++ NameWheel/MlpNetwork.h
@@ -3,34 +3,34 @@
 //#define __DEBUGING__ true
 
 /**
- *	MIT License
+ *  MIT License
+ *  
+ *  Copyright (c) 2020 Hata.Rotom https://github.com/BYOUINZAKA
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  *
- *	Copyright (c) 2020 Hata.Rotom https://github.com/BYOUINZAKA
- *
- *	Permission is hereby granted, free of charge, to any person obtaining a copy
- *	of this software and associated documentation files (the "Software"), to deal
- *	in the Software without restriction, including without limitation the rights
- *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *	copies of the Software, and to permit persons to whom the Software is
- *	furnished to do so, subject to the following conditions:
- *
- *	The above copyright notice and this permission notice shall be included in all
- *	copies or substantial portions of the Software.
- *
- *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *	SOFTWARE.
- *
- *	@file		MlpNetwork.h
- *	@brief		A header file of the MlpNetwork class,
- *				contains its declaration and definition.
- *	@author		Hata
- *	@version	beta-1.0
- *	@date		2020-02-03
+ *  @file       MlpNetwork.h
+ *  @brief      A header file of the MlpNetwork class,
+ *              contains its declaration and definition.
+ *  @author     Hata
+ *  @version    beta-1.0
+ *  @date       2020-02-03
  */
 
 #ifdef __DEBUGING__
@@ -43,16 +43,14 @@
 namespace kodo
 {
     /**
-     *	@brief		Multi-Layer Perceptron Network class.
-     *				A class that encapsulates a Multi-Layer Perceptron,
-     *				it can forward and backward.
-     *	@example	You can create a 24-input and 3-output ANN has 4 hidden layers like this:
-     *	@code
-     *				kodo::MlpNetwork<24,
-     *					3,
-     *					kodo::Hidden<12, 24, 48, 96>>
-     *					mlp("LeakyReLU", "Softmax", "crossentropy");
-     *	@endcode
+     *  @brief      Multi-Layer Perceptron Network class.
+     *              A class that encapsulates a Multi-Layer Perceptron,
+     *              it can forward and backward.
+     *  @example    You can create a 24-input and 3-output ANN has 4 hidden layers like this:
+     *  @code
+     *              kodo::MlpNetwork<24, 3, kodo::Hidden<12, 24, 48, 96>>
+     *              mlp("LeakyReLU", "Softmax", "crossentropy");
+     *  @endcode
      */
     template <unsigned InputCount, unsigned OutputCount, class T>
     class MlpNetwork {
@@ -63,24 +61,24 @@ namespace kodo
         using LossFunction = std::unique_ptr<LossFunc>;
         constexpr static const size_t HiddenCount = HiddensType::HIDDENS_QTY;
         /**
-         * @brief		Constructor.
-         * @param		learningRate	Learningrate of the network.
-         * @param		middle_type		Type of hidden layers activation functions
-         * @param		out_type		Type of out layer activation functions
-         * @param		loss_type		Set up the target function and the loss function
-         * @exception	NotFunctionError
+         * @brief       Constructor.
+         * @param       learningRate    Learningrate of the network.
+         * @param       middle_type     Type of hidden layers activation functions
+         * @param       out_type        Type of out layer activation functions
+         * @param       loss_type       Set up the target function and the loss function
+         * @exception   NotFunctionError
          */
-        MlpNetwork(double		learningRate,
-                   const char*	middle_type,
-                   const char*	out_type,
-                   const char*	loss_type);
+        MlpNetwork(double       learningRate,
+                   const char*  middle_type,
+                   const char*  out_type,
+                   const char*  loss_type);
 
     private:
-        double				_learningRate;
-        HiddensType			_hidden;
-        ActFunction			_middleFunc;
-        ActFunction			_outputFunc;
-        LossFunction		_lossFunc;
+        double              _learningRate;
+        HiddensType         _hidden;
+        ActFunction         _middleFunc;
+        ActFunction	        _outputFunc;
+        LossFunction        _lossFunc;
         std::vector<Vector> _in;
         std::vector<Vector> _out;
         std::vector<Vector> _bias;
@@ -88,9 +86,9 @@ namespace kodo
         std::vector<Vector> _loss;
 
     public:
-        const Vector&	Forward(const Vector &input);
-        void			Backward(const Vector &real);
-        std::ostream&	Print(std::ostream &os);
+        const Vector&   Forward(const Vector &input);
+        void            Backward(const Vector &real);
+        std::ostream&   Print(std::ostream &os);
         double t(const Vector& real) {
             auto r = _lossFunc->proto(_out.back(), real);
             double sum = 0.0;
